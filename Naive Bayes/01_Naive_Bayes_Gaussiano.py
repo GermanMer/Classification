d504e9889c4c006d822e9e3211bb621789bc8f04
German Mercader <121909886+GermanMer@users.noreply.github.com>
2023-10-10 11:05:43 -0300

    Add files via upload

--- Naive Bayes/01_Naive_Bayes_Gaussiano.py
+++ Naive Bayes/01_Naive_Bayes_Gaussiano.py
@@ -1,1 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score, classification_report
 
+# Cargar el conjunto de datos Iris
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+# Dividir el conjunto de datos en entrenamiento y prueba
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Crear un clasificador Naive Bayes Gaussiano
+gnb = GaussianNB()
+
+# Entrenar el clasificador en el conjunto de entrenamiento
+gnb.fit(X_train, y_train)
+
+# Realizar predicciones en el conjunto de prueba
+y_pred = gnb.predict(X_test)
+
+# Calcular la precisión del modelo
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Precisión del modelo Naive Bayes: {accuracy * 100:.2f}%')
+
+# Mostrar el informe de clasificación detallado
+class_report = classification_report(y_test, y_pred, target_names=iris.target_names)
+print('Informe de clasificación:')
+print(class_report)
+
+# Agregar una espera para que la ventana no se cierre automáticamente en Windows
+input("Presiona Enter para salir...")
