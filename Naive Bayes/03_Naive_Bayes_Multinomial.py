fa4190b6dee34961a2dd3285d3458aaf50ca5e66
German Mercader <121909886+GermanMer@users.noreply.github.com>
2023-10-10 11:06:31 -0300

    Add files via upload

---
+++ Naive Bayes/03_Naive_Bayes_Multinomial.py
@@ -0,0 +1,39 @@
+import numpy as np
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, classification_report
+
+# Cargar el conjunto de datos "20 Newsgroups" (seleccione las categorías que desee)
+categories = ['sci.med', 'sci.space', 'rec.autos', 'talk.politics.guns']
+newsgroups = fetch_20newsgroups(subset='all', categories=categories, remove=('headers', 'footers', 'quotes'))
+
+# Preprocesar los datos de texto utilizando CountVectorizer
+vectorizer = CountVectorizer(stop_words='english')
+X = vectorizer.fit_transform(newsgroups.data)
+y = newsgroups.target
+
+# Dividir el conjunto de datos en entrenamiento y prueba
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Crear un clasificador Naive Bayes Multinomial
+mnb = MultinomialNB()
+
+# Entrenar el clasificador en el conjunto de entrenamiento
+mnb.fit(X_train, y_train)
+
+# Realizar predicciones en el conjunto de prueba
+y_pred = mnb.predict(X_test)
+
+# Calcular la precisión del modelo
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Precisión del modelo Naive Bayes Multinomial: {accuracy * 100:.2f}%')
+
+# Mostrar el informe de clasificación detallado
+class_report = classification_report(y_test, y_pred, target_names=newsgroups.target_names)
+print('Informe de clasificación:')
+print(class_report)
+
+# Agregar una espera para que la ventana no se cierre automáticamente en Windows
+input("Presiona Enter para salir...")
