6262a61cc35969cba33d91723d10d100cf6711eb
German Mercader <121909886+GermanMer@users.noreply.github.com>
2023-10-11 10:33:05 -0300

    Add files via upload

---
+++ Naive Bayes/07_Naive_Bayes_Bernoulli_ParametroBinarize.py
@@ -0,0 +1,38 @@
+#En este ejemplo, hemos creado un clasificador Naive Bayes Bernoulli y hemos especificado el hiperparámetro binarize con un valor de umbral de 2.5. Esto significa que cualquier característica en los datos que tenga un valor mayor que 2.5 se considerará como 1, mientras que las características con valores iguales o menores que 2.5 se considerarán como 0.
+#La elección del valor de umbral 2.5 es arbitraria y puede ajustarse según los requisitos de tu problema y la naturaleza de los datos. El ajuste de este valor puede influir en el rendimiento del modelo en función de cómo deseas que se binaricen las características.
+
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.metrics import accuracy_score, classification_report
+
+# Cargar el conjunto de datos Iris
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+# Dividir el conjunto de datos en entrenamiento y prueba
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Crear un clasificador Naive Bayes Bernoulli con el parámetro binarize
+binarize_threshold = 2.5  # Umbral para binarizar las características
+bnb = BernoulliNB(binarize=binarize_threshold)
+
+# Entrenar el clasificador en el conjunto de entrenamiento
+bnb.fit(X_train, y_train)
+
+# Realizar predicciones en el conjunto de prueba
+y_pred = bnb.predict(X_test)
+
+# Calcular la precisión del modelo
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Precisión del modelo Naive Bayes Bernoulli: {accuracy * 100:.2f}%')
+
+# Mostrar el informe de clasificación detallado
+class_report = classification_report(y_test, y_pred, target_names=iris.target_names)
+print('Informe de clasificación:')
+print(class_report)
+
+# Agregar una espera para que la ventana no se cierre automáticamente en Windows
+input("Presiona Enter para salir...")
